ef1ec38a7ebb81a856d9d3facd108cd27d099afd
hfukasaka <hfukasaka@geek.co.jp>
2020-02-25 16:44:28 +0900

    [update] ・components stone セルとしても扱うように修正

--- src/components/stone/styled.tsx
+++ src/components/stone/styled.tsx
@@ -1,14 +1,24 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-    width: 20px;
-    height: 20px;
-    border: 1px solid #000;
-    border-radius: 50%;
-    &.black {
+    position: relative;
+    width: 40px;
+    height: 40px;
+    &:before {
+        content: "";
+        position: absolute;
+        display: block;
+        width: 20px;
+        height: 20px;
+        border-radius: 50%;
+        top: 0;
+        left: 0;
+        transform: translate(50%, 50%);
+    }
+    &.black:before {
         background-color: #000;
     }
-    &.white {
+    &.white:before {
         background-color: #fff;
     }
 `;
